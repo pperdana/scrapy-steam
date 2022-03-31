0f9825c9556850b42b0340a7c21cf26821be8ade
Panji-Perdana <93533992+Panji200@users.noreply.github.com>
2022-03-31 10:17:38 +0700

    Add files via upload

---
+++ spiders/best_selling.py
@@ -0,0 +1,10 @@
+import scrapy
+
+
+class BestSellingSpider(scrapy.Spider):
+    name = 'best_selling'
+    allowed_domains = ['store.steampowered.com']
+    start_urls = ['https://store.steampowered.com/search/?filter=topsellers']
+
+    def parse(self, response):
+        pass
